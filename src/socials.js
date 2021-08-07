22c8b704dbf421fa44e3345bae262c33f7f71a25
Atakan Ermiş <ataerms@gmail.com>
2021-08-07 12:47:52 +0300

    update mail address

--- src/socials.js
+++ src/socials.js
@@ -1,39 +1,39 @@
 const socials = [
-	{
-		name: "Twitter",
-		icon: "icon-twitter",
-		link: "https://twitter.com/atakan_ermis"
-	},
-	{
-		name: "Reddit",
-		icon: "icon-reddit",
-		link: "https://www.reddit.com/user/AtakanErmis"
-	},
-	{
-		name: "Github",
-		icon: "icon-github",
-		link: "https://github.com/AtakanErmis"
-	},
-	{
-		name: "Telegram",
-		icon: "icon-telegram",
-		link: "https://t.me/AtakanErmis"
-	},
-	{
-		name: "Last.FM",
-		icon: "icon-lastfm",
-		link: "https://www.last.fm/user/AtakanErmis"
-	},
-	{
-		name: "Spotify",
-		icon: "icon-spotify",
-		link: "https://open.spotify.com/user/5c9in3cvvftwlkbaeqcfvsdok?si=g6Llj8FyQTmMMN40HUgTOg"
-	},
-	{
-		name: "E-Mail",
-		icon: "icon-email",
-		link: "mailto:ataerms@gmail.com"
-	}
+  {
+    name: "Twitter",
+    icon: "icon-twitter",
+    link: "https://twitter.com/atakan_ermis",
+  },
+  {
+    name: "Reddit",
+    icon: "icon-reddit",
+    link: "https://www.reddit.com/user/AtakanErmis",
+  },
+  {
+    name: "Github",
+    icon: "icon-github",
+    link: "https://github.com/AtakanErmis",
+  },
+  {
+    name: "Telegram",
+    icon: "icon-telegram",
+    link: "https://t.me/AtakanErmis",
+  },
+  {
+    name: "Last.FM",
+    icon: "icon-lastfm",
+    link: "https://www.last.fm/user/AtakanErmis",
+  },
+  {
+    name: "Spotify",
+    icon: "icon-spotify",
+    link: "https://open.spotify.com/user/5c9in3cvvftwlkbaeqcfvsdok?si=g6Llj8FyQTmMMN40HUgTOg",
+  },
+  {
+    name: "E-Mail",
+    icon: "icon-email",
+    link: "mailto:me@atakanermis.com",
+  },
 ];
 
-export default socials
+export default socials;
